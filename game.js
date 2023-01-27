bbd413b9ea9a97b01cf36b9b2b4072ddfc20e85b
DariaRaiRai <dari.shamrai@gmail.com>
2023-01-27 11:38:38 -0500

    add game over and h2 when hit an obstacle

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const dino = document.querySelector(".dino");
   const grid = document.querySelector(".grid");
+  const alert = document.getElementById("alert");
   let isJumping = false;
   let gravity = 0.9;
+  let isGameOver = false;
 
   function control(e) {
     if (e.keyCode === 32) {
@@ -45,20 +47,25 @@ document.addEventListener("DOMContentLoaded", () => {
     let randomTime = Math.random() * 4000;
     let obstaclePosition = 1000;
     const obstacle = document.createElement("div");
-    obstacle.classList.add("obstacle");
+    if (!isGameOver) obstacle.classList.add("obstacle");
     grid.appendChild(obstacle);
     obstacle.style.left = obstaclePosition + "px";
 
     let timerId = setInterval(function () {
-      if (obstaclePosition === 0) {
+      if (obstaclePosition === 0 && obstaclePosition < 60 && position < 60) {
         clearInterval(timerId);
-        alert("Game Over");
+        alert.innerHTML = "Game Over";
+        isGameOver = true;
+        //remove all children
+        while (grid.firstChild) {
+          grid.removeChild(grid.lastChild);
+        }
       }
 
       obstaclePosition -= 10;
       obstacle.style.left = obstaclePosition + "px";
     }, 20);
-    setTimeout(generateObstacles, randomTime);
+    if (!isGameOver) setTimeout(generateObstacles, randomTime);
   }
   generateObstacles();
 });
