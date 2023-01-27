37225242c82ff9414eb28100f3b1b7ba5978cc40
DariaRaiRai <dari.shamrai@gmail.com>
2023-01-27 11:32:28 -0500

    add main element in js file

--- game.js
+++ game.js
@@ -1,1 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const dino = document.querySelector(".dino");
+  const grid = document.querySelector(".grid");
+  let isJumping = false;
+  let gravity = 0.9;
 
+  function control(e) {
+    if (e.keyCode === 32) {
+      if (!isJumping) {
+        isJumping = true;
+        jump();
+      }
+    }
+  }
+  document.addEventListener("keyup", control);
+
+  let position = 0;
+  function jump() {
+    let count = 0;
+    let timerId = setInterval(function () {
+      //move down
+      if (count === 15) {
+        clearInterval(timerId);
+        let downTimerId = setInterval(function () {
+          if (count === 0) {
+            clearInterval(downTimerId);
+            isJumping = false;
+          }
+          position -= 5;
+          count--;
+          position = position * gravity;
+          dino.style.bottom = position + "px";
+        }, 20);
+      }
+
+      //move up
+      position += 30;
+      count++;
+      position = position * gravity;
+      dino.style.bottom = position + "px";
+    }, 20);
+  }
+});
